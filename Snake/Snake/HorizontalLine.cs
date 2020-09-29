bc00ed0ced15d2160e9b6016c42033baa85768cd
Dmitry_Meshkov <dm_91@bk.ru>
2020-09-29 16:00:29 +0300

    Добавлен класс Figure

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        List<Point> pList;
+
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
@@ -20,12 +20,6 @@ namespace Snake
                 pList.Add(p);
             }
         }
-        public void Draw()
-        {
-            foreach(Point p in pList)
-            {
-                p.Draw();
-            }
-        }
+
     }
 }
