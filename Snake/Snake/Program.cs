4463686e415f073a8dd3ee3b519f2c38c1e3869f
Dmitry_Meshkov <dm_91@bk.ru>
2020-09-30 16:40:41 +0300

    Убран дублирующийся метод Move()

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -49,8 +49,7 @@ namespace Snake
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(300);
-                snake.Move();
+                Thread.Sleep(100);
             }
 
             Console.ReadLine();
